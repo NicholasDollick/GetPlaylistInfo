01e46b14f290ee916ef42d54c0db0610465226d1
NicholasDollick <nidollick@gmail.com>
2018-07-18 22:22:29 -0700

    added messy excel support

--- KeyBPM/Form1.Designer.cs
+++ KeyBPM/Form1.Designer.cs
@@ -28,12 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.accountLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.countryLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.playlistsListBox = new System.Windows.Forms.ListBox();
+            this.authButton = new System.Windows.Forms.Button();
+            this.URI = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Tracks Being Added:";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(63, 18);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox.TabIndex = 34;
+            this.pictureBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nameLabel);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.accountLabel);
+            this.groupBox1.Controls.Add(this.emailLabel);
+            this.groupBox1.Controls.Add(this.countryLabel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(9, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 113);
+            this.groupBox1.TabIndex = 33;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(73, 25);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(10, 13);
+            this.nameLabel.TabIndex = 14;
+            this.nameLabel.Text = "-";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Account:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Email:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Name:";
+            // 
+            // accountLabel
+            // 
+            this.accountLabel.AutoSize = true;
+            this.accountLabel.Location = new System.Drawing.Point(73, 79);
+            this.accountLabel.Name = "accountLabel";
+            this.accountLabel.Size = new System.Drawing.Size(10, 13);
+            this.accountLabel.TabIndex = 17;
+            this.accountLabel.Text = "-";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(73, 61);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(10, 13);
+            this.emailLabel.TabIndex = 16;
+            this.emailLabel.Text = "-";
+            // 
+            // countryLabel
+            // 
+            this.countryLabel.AutoSize = true;
+            this.countryLabel.Location = new System.Drawing.Point(73, 43);
+            this.countryLabel.Name = "countryLabel";
+            this.countryLabel.Size = new System.Drawing.Size(10, 13);
+            this.countryLabel.TabIndex = 15;
+            this.countryLabel.Text = "-";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Country:";
+            // 
+            // playlistsListBox
+            // 
+            this.playlistsListBox.FormattingEnabled = true;
+            this.playlistsListBox.Location = new System.Drawing.Point(232, 46);
+            this.playlistsListBox.Name = "playlistsListBox";
+            this.playlistsListBox.Size = new System.Drawing.Size(190, 30);
+            this.playlistsListBox.TabIndex = 32;
+            // 
+            // authButton
+            // 
+            this.authButton.Location = new System.Drawing.Point(34, 247);
+            this.authButton.Name = "authButton";
+            this.authButton.Size = new System.Drawing.Size(142, 28);
+            this.authButton.TabIndex = 31;
+            this.authButton.Text = "Authenticate Session";
+            this.authButton.UseVisualStyleBackColor = true;
+            this.authButton.Click += new System.EventHandler(this.authButton_Click);
+            // 
+            // URI
+            // 
+            this.URI.Location = new System.Drawing.Point(315, 200);
+            this.URI.Name = "URI";
+            this.URI.Size = new System.Drawing.Size(136, 20);
+            this.URI.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(249, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "Playlist URI";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(315, 351);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 38;
+            this.startButton.Text = "button1";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 422);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.URI);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.playlistsListBox);
+            this.Controls.Add(this.authButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label accountLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label countryLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox playlistsListBox;
+        private System.Windows.Forms.Button authButton;
+        private System.Windows.Forms.TextBox URI;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button startButton;
     }
 }
 
--- KeyBPM/Form1.cs
+++ KeyBPM/Form1.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Enums;
+using SpotifyAPI.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +20,110 @@ namespace KeyBPM
 {
     public partial class Form1 : Form
     {
+        private SpotifyWebAPI spotify;
+        private PrivateProfile profile;
+        private OpenFileDialog ofd = new OpenFileDialog();
+        private string[] keyDict = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+        //private string filePath = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void authButton_Click(object sender, EventArgs e)
+        {
+           Task.Run(() => RunAuthentication());
+        }
+
+        private async void RunAuthentication()
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                "http://localhost", 8000, "78c190180d5e4e79baf28a7ad4c04018",
+                Scope.UserReadPrivate | Scope.UserReadEmail | Scope.PlaylistReadPrivate | Scope.UserLibraryRead |
+                Scope.PlaylistModifyPublic | Scope.UserFollowRead | Scope.UserReadBirthdate | Scope.UserTopRead | Scope.PlaylistReadCollaborative |
+                Scope.UserReadRecentlyPlayed | Scope.UserReadPlaybackState | Scope.UserModifyPlaybackState);
+
+            try
+            {
+                spotify = await webApiFactory.GetWebApi();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (spotify == null)
+                return;
+
+            InitialSetup();
+        }
+
+        private async void InitialSetup()
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(InitialSetup));
+                return;
+            }
+
+            authButton.Enabled = false;
+            profile = await spotify.GetPrivateProfileAsync();
+            nameLabel.Text = profile.DisplayName;
+            countryLabel.Text = profile.Country;
+            emailLabel.Text = profile.Email;
+            accountLabel.Text = profile.Product;
+
+            if (profile.Images != null && profile.Images.Count > 0)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    byte[] imageBytes = await wc.DownloadDataTaskAsync(new Uri(profile.Images[0].Url));
+                    using (MemoryStream stream = new MemoryStream(imageBytes))
+                        pictureBox.Image = System.Drawing.Image.FromStream(stream);
+                }
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            SearchItem song = new SearchItem();
+            ErrorResponse response = new ErrorResponse();
+            List<FullTrack> tracks = new List<FullTrack>();
+
+            var playlist = spotify.GetPlaylistTracks(profile.Id, "6crTEjprqpnhmF04kjWEWu"); //this ID is hardcoded for testing. Please fix this
+            playlist.Items.ForEach(track => tracks.Add(track.Track));
+
+            var analysis = spotify.GetAudioAnalysis(tracks[0].Id);
+            MessageBox.Show(tracks[0].Name);
+            //analysis.Track.ForEach(single => Console.WriteLine(single.Tempo));
+            //analysis.Track.ForEach(single => Console.WriteLine(single.Key));
+
+            //MessageBox.Show(analysis.Track.Key.ToString());
+            //MessageBox.Show(analysis.Track.Tempo.ToString());
+            //MessageBox.Show(analysis.Track.Mode.ToString());
+
+            var workbook = new XSSFWorkbook();
+            var sheet = workbook.CreateSheet("Sheet1");
+
+            sheet.CreateRow(0).CreateCell(0).SetCellValue("This Is A Test");
+
+            int x = 1;
+
+            for (int i = 1; i <= 15; i++) //row
+            {
+                IRow row = sheet.CreateRow(i);
+
+                for (int j = 0; j < 15; j++) //column
+                {
+                    row.CreateCell(j).SetCellValue(x++);
+                }
+            }
+
+            var sw = File.Create("test.xlsx");
+
+            workbook.Write(sw);
+            sw.Close();
         }
     }
 }
